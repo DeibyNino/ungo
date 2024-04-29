6d392067aac7a15f0dc50c30df0f16b53d45b0d7
deibyNGargces <deiby.ninog@uniagustiniana.edu.co>
2024-04-29 18:33:42 -0500

    se creao el compoente welcome

---
+++ src/components/Home/Welcome.jsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <div className="section section1">
+      <h1>Welcome section</h1>
+    </div>
+  );
+};
+
+export default Welcome;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import Welcome from "../components/Home/Welcome";
 import "../styles/HomeStyles.css";
 
 const Home = () => {
   return (
     <div>
       <h1 className="titulo-home">Home</h1>
-      <div className="section section1">seccion 1</div>
+      <Welcome />
       <div className="section section2">seccion 2</div>
       <div className="section section3">seccion 3</div>
     </div>
