e92a6bc96523c6ece12f00d2b06ee916a5aa0263
Paula <paulaandrearoldangarcia045@gmail.com>
2024-04-30 18:23:43 -0500

    se agrego componente Contactform

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import "../styles/ContactStyles.css";
+import ContactForm from "../components/Contact/ContactForm";
 
 const Contact = () => {
   return (
     <div className="main-contact">
+      <ContactForm/>
       <h1 className="titulo1 titulo">Titulo </h1>
       <h1 className="titulo">Contact Page</h1>
     </div>
