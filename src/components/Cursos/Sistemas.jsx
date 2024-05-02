0972069a58e53e2c83b820d4f65800fd1f613826
deibi66 <papuchodeibis@email.com>
2024-05-02 18:43:27 -0500

    sm lo acabo de hacer

--- src/components/Cursos/Sistemas.jsx
+++ src/components/Cursos/Sistemas.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Sistemas = () => {
   return (
-    <div>Curso de Sistemas</div>
+    <div>Curso de Sistemas
+        <a href="https://cree.uniagustiniana.edu.co/ingenieria-en-telecomunicaciones-tagaste/">ejemplo</a>    </div>
   )
 }
 
