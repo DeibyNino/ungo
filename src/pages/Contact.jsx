ef58c76a238a1408c1d5aa0c46d3dbda37a5251a
Paula <paulaandrearoldangarcia045@gmail.com>
2024-05-11 11:35:04 -0500

    se agrego titulo y barritas esas

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,32 @@ const Contact = () => {
   return (
     <div className="main-contact">
       <ContactForm/>
-      <h1 className="titulo1 titulo">Titulo </h1>
-      <h1 className="titulo">Contact Page</h1>
+      <h1 className="titulo1 titulo">CONTACTANOS </h1>
+      <section className="contact">
+      <div className="data-contact">
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <form className="form-contact">
+                        <div className="input-box-contact">
+                            <input type="text" name="username" ></input>
+                           </div>  
+                           <div className="input-box-contact">
+                            <input type="text" name="correoelectronico"></input>
+                            <div className="input-box-contact">
+                            <input type="text" placeholder="Asunto"></input>
+                           
+                        </div>
+            
+                        <div className="input-box-contact">
+                            <textarea placeholder="Escribe tu mensaje..."></textarea>
+                        </div>
+                        <button type="submit">contactanos</button>
+                           
+                        </div>
+                     </form>
+      </section>
     </div>
   );
 };
