0a9ccbaedd009b18a76523782e65bf6dc1c73f35
Deibysoyyo <deibysoyyo@hotmail.com>
2024-05-25 12:19:47 -0500

    menu responsive

--- src/components/NavBar/Footer.jsx
+++ src/components/NavBar/Footer.jsx
@@ -3,7 +3,7 @@ import { ImFacebook2 } from "react-icons/im";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer-main">
       <div>
         <a
           href="httpsg://www.facebook.com/UNGO-Universidad-ONG-108101394678013"
@@ -13,7 +13,39 @@ const Footer = () => {
           <ImFacebook2 color="#00f" size={50} />
         </a>
       </div>
-      <p>© 2021 UNGO. Todos los derechos reservados.</p>
+      <div className="footer">
+        <nav>
+          <ul
+            style={{
+              listStyleType: "none",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+              gap: "1rem",
+            }}
+          >
+            <li>
+              <a href="/">Inicio</a>
+            </li>
+            <li>
+              <a href="/sedes-page">Sedes</a>
+            </li>
+            <li>
+              <a href="/contact-page">Contactanos</a>
+            </li>
+            <li>
+              <a href="/cursos-page">Cursos</a>
+            </li>
+            <li>
+              <a href="/about-page">Nosotros</a>
+            </li>
+            <li>
+              <a href="/donaciones-page">Donaciones</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <p>© 2024 UNGO. Todos los derechos reservados.</p>
     </div>
   );
 };
