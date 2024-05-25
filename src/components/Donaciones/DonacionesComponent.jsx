9392fdb7a1e7a3fd2ad6c65070741a9218c221cc
johaNkjk <johanalonso1177@gmail.com>
2024-05-25 12:45:16 -0500

    subo mi parte de donaciones

--- src/components/Donaciones/DonacionesComponent.jsx
+++ src/components/Donaciones/DonacionesComponent.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import ungo from "../../assets/images/Donaciones/ungo.png"
+
+
 
 const DonacionesComponent = () => {
   return (
-    <div>
-      <h1>Donaciones </h1>
+    <div class="donaciones-rolito">
+      <div class="imagen-ungo">
+        <img src={ungo} alt="" />
+      </div>
+      <div className="title-donaciones">
+        <h1>Donaciones </h1>
+      </div>
+      <div className="introduccion">
       <p>
         La Fundación UNGO “estudia bien” tiene como misión el desarrollo de
         proyectos educativos, culturales y empresariales en Colombia, con
@@ -11,10 +20,29 @@ const DonacionesComponent = () => {
         adultos íntegros, autónomos con valores y principios para la formación
         de una mejor sociedad.
       </p>
-      <h1>Contactanos si deseas realizar tu aporte</h1>
-      <p>Correo: test@email.com </p>
-      <p>Telefono: +57 1234567890</p>
-      <button>donar</button>
+      </div>
+      <div className="subtitulo">
+        <h1>Contactanos si deseas realizar tu aporte</h1>
+      </div>
+
+      <div className="form-donaciones">
+
+        <div class="input-box">
+          <label>Email:</label>
+          <input type="gmail" placeholder="gmail" required />
+        </div>
+        <div class="input-box">
+          <label>Password</label>
+          <input type="password" placeholder="password" required />
+        </div>
+        <div class="entrada-chack">
+          <input type="checkbox" />
+          <label for="aceptar"> aceptar condicones </label>
+        </div>
+        <div class="donacion">
+          <button> donar -</button>
+        </div>
+      </div>
     </div>
   );
 };
