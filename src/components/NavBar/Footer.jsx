e595058f4128ec3d66af6265d4b5d74d432116bb
Deibysoyyo <deibysoyyo@hotmail.com>
2024-06-08 11:39:20 -0500

    algunos asuntos

--- src/components/NavBar/Footer.jsx
+++ src/components/NavBar/Footer.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 import { ImFacebook2 } from "react-icons/im";
+import { ImYoutube2 } from "react-icons/im";
+import { ImInstagram } from "react-icons/im";
+import { AiFillTikTok } from "react-icons/ai";
 import "../../styles/FooterStyles.css";
 import logo from "../../assets/ImageNavBar/Logo.png";
 
 const Footer = () => {
   return (
     <div className="footer-main">
-      <div>
-        <a
-          href="httpsg://https://www.facebook.com/profile.php?id=61555748215327"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <ImFacebook2 color="#00f" size={50} />
-        </a>
-      </div>
       <div className="footer">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
         <nav>
           <ul
             style={{
               listStyleType: "none",
               display: "flex",
-              flexDirection: "column",
               justifyContent: "space-around",
-              gap: "1rem",
+              gap: "0.5rem",
             }}
           >
             <li>
@@ -46,6 +42,44 @@ const Footer = () => {
             </li>
           </ul>
         </nav>
+        <div>
+          <div>
+            <p> Contáctanos: 3160472448</p>
+            <p> Gmail: estudiaungo@gmail.com</p>
+            <p> Direccion: Cl 29 Sur No.19d-40</p>
+            <p> Whatsapp: +57 3185233294</p>
+          </div>
+          <div className="redes">
+            <a
+              href="httpsg://https://www.facebook.com/profile.php?id=61555748215327"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <ImFacebook2 color="#fff" size={50} />
+            </a>
+            <a
+              href="httpsg://https://www.facebook.com/profile.php?id=61555748215327"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <ImYoutube2 color="#fff" size={50} />
+            </a>
+            <a
+              href="httpsg://https://www.facebook.com/profile.php?id=61555748215327"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <ImInstagram color="#fff" size={50} />
+            </a>
+            <a
+              href="httpsg://https://www.facebook.com/profile.php?id=61555748215327"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <AiFillTikTok color="#fff" size={50} />
+            </a>
+          </div>
+        </div>
       </div>
       <p>© 2024 UNGO. Todos los derechos reservados.</p>
     </div>
